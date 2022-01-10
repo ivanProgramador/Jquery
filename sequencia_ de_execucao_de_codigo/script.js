625e79c44c6c5b058a58afe3da76305950cbb5d2
ivan <ivan-guita@hotmail.com>
2022-01-10 09:41:12 -0300

    precedencia de codigo js compreendida e implementada com funções que esperam o carregamento de todo o douemento para executar o codigo

---
+++ sequencia_ de_execucao_de_codigo/script.js
@@ -0,0 +1,16 @@
+	/*passando a função como parametro mais nesse caso a função deve ser anonima para que não aconteçam erros de seleção embora a referencia ao js
+      esteja na read antes de qualquer elemento existir no codigo abaixo o js espera toda a apgina carregar e depois executa pos isso todos os scripts
+      daqui pra frente serão executados dentro desta função anonima. 
+	*/
+	$(document).ready(function(){
+
+
+
+
+
+
+	});
+
+	
+
+
